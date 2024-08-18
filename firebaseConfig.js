447c77f435ce8c1694420dd484d24192127c9029
atikaabdulhayee <atikaabdulhayee51@gmail.com>
2024-08-18 00:44:07 -0700

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,14 +3,14 @@ import { getDatabase, goOnline, goOffline } from "firebase/database";
 import 'react-native-get-random-values';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyABgpCDuSzEw6_Zc6pDUx7sRpmHXuKQr0c",
-  authDomain: "rental-system-2f6b4.firebaseapp.com",
-  databaseURL: "https://rental-system-2f6b4-default-rtdb.firebaseio.com",
-  projectId: "rental-system-2f6b4",
-  storageBucket: "rental-system-2f6b4.appspot.com",
-  messagingSenderId: "862030347364",
-  appId: "1:862030347364:web:e640b3ca13d707f34ff371",
-  measurementId: "G-JJFY2KT65V"
+  apiKey: "XXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXX",
+  projectId: "XXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXX",
+  appId: "XXXXXXXXXXXX",
+  measurementId: "XXXXXXXXXXXX"
 };
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
